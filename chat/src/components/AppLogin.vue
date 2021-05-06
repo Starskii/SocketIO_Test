654275934f795bdf35a1ec357211b74c72bc4e3d
Starskii <jakeylakie@yahoo.com>
2021-05-06 06:24:34 -0400

    Styled login page

--- chat/src/components/AppLogin.vue
+++ chat/src/components/AppLogin.vue
@@ -3,67 +3,51 @@
   <div id="app">
   <v-app id="inspire">
     <v-card width="400px" class="mx-auto mt-5">
-    <v-card-title>
+    <v-card-title width="400px">
 
-      <v-tabs
-          v-model="tab"
-          align-with-title
-        >
-          <v-tabs-slider color="blue"></v-tabs-slider>
+     <v-tabs
+      v-model="tab"
+      fixed-tabs
+    >
+      <v-tab
+        v-for="item in items"
+        :key="item.tab"
+      >
+        {{ item.tab }}
+      </v-tab>
+    </v-tabs>
 
-          <v-tab
-            v-for="item in items"
-            :key="item"
-          >
-            {{ item }}
-          </v-tab>
-        </v-tabs>
-
-         <v-tabs-items v-model="tab">
+    <v-tabs-items v-model="tab">
       <v-tab-item
         v-for="item in items"
-        :key="item"
+        :key="item.tab"
       >
-        <v-card flat>
-          <v-card-text v-text="text"></v-card-text>
+        <v-card flat width="400px">
+          <v-card-text>
+            
+            <v-form
+            v-for="form in item.content.forms"
+            :key="form.name"
+            >
+
+            <v-text-field
+            :label=form.name
+            :type=form.type
+            :placeholder=form.placeholder
+            :v-model="form.model"
+            >
+            </v-text-field>
+
+            </v-form>
+          {{ message }} 
+
+        <v-btn  elevation= "18" color= "blue" top :style="{left: '42%', transform:'translateX(-50%)'}" @click="createAccount">{{item.content.buttonText}}</v-btn>
+
+          </v-card-text>
         </v-card>
       </v-tab-item>
     </v-tabs-items>
-
     </v-card-title> 
-    <v-card-text>
-      <v-form>
-        <v-text-field 
-          label="Email" 
-          prepend-icon="mdi-account-circle"
-          input name="Email" id="Email" type="text"  placeholder="Email" v-model="userEmail" 
-        />
-      </v-form>
-      
-      <v-form>
-        <v-text-field 
-        label="Display Name" 
-        prepend-icon="mdi-account-circle"
-        input name="DisplayName" id="DisplayName" type="text"  placeholder="Display Name" v-model="displayName" 
-        />
-      </v-form>
-
-      <v-form>
-        <v-text-field  
-        input name="Password" id="Password"  placeholder="Password" v-model="userPassword" 
-          type="password"
-          label="Password" 
-          prepend-icon="mdi-lock"
-        />
-      </v-form>
-    </v-card-text>
-     {{ message }} 
-      <v-divider></v-divider>
-    <v-card-actions>
-        <v-btn  elevation= "18" color= "blue" :disabled="noInput" @click="createAccount">SignUp</v-btn>
-          <v-spacer></v-spacer>
-        <v-btn elevation="18" color ="blue" :disabled="noInput" @click="authenticate">LogIn</v-btn>
-    </v-card-actions>
     </v-card>
   </v-app>
 </div>
@@ -79,6 +63,31 @@ import { FirebaseAuth } from "@firebase/auth-types";
 
 @Component
 export default class AppLogin extends Vue {
+
+  data () {
+      return {
+        tab: null,
+        items: [
+          { 
+            tab: 'Login', content: {
+              forms: [
+                {name: "Email", type:"text", placeholder: "Email", model: ""},
+                {name: "Password", type: "password", placeholder: "Password", model: ""}
+              ],
+              buttonText: "Login"
+          } },
+          { 
+            tab: 'Sign Up', content: {
+              forms: [
+                {name: "Display Name", type: "text", placeholder: "Display Name", model: ""},
+                {name: "Email", type:"text", placeholder: "Email", model: ""},
+                {name: "Password", type: "password", placeholder: "Password", model: ""}
+              ],
+              buttonText: "SignUp"
+          } }
+        ],
+      }
+    }
  
   readonly $appAuth!: FirebaseAuth;
   readonly $appDB!: FirebaseFirestore;
@@ -86,10 +95,6 @@ export default class AppLogin extends Vue {
   private userPassword = "";
   private displayName = "";
   private message = "";
-  private items: string[] = [
-    "Login",
-    "Sign Up"
-  ]
   $router: any;
 
   get noInput(): boolean {
